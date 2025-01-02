e4b4687dcf5c244a650acae88b790125d5f0b6f1
Lucas Jeanes <lucasjeanes@gmail.com>
2025-01-02 18:57:43 +0000

    Created ServiceLayer application shell

---
+++ src/main/java/com/ie/cicd_project_2_servicelayer/CicdProject2ServiceLayerApplication.java
@@ -0,0 +1,13 @@
+package com.ie.cicd_project_2_servicelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProject2ServiceLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProject2ServiceLayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ie/cicd_project_2_servicelayer/CicdProject2ServiceLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ie.cicd_project_2_servicelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProject2ServiceLayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
